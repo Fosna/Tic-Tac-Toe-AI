0c8aaa91daa0fc0367b16e3567bd02fac3b5fb59
Fosna <vedran.marsic.ka@gmail.com>
2016-03-29 19:34:39 +0200

    Game as ES2015 class.

--- scripts/game.js
+++ scripts/game.js
@@ -2,43 +2,45 @@ import ui from './ui.js';
 import State from './state.js';
 
 
-/*
- * Constructs a game object to be played
- * @param autoPlayer [AIPlayer] : the AI player to be play the game with
- */
-var Game = function(autoPlayer) {
-
-    //public : initialize the ai player for this game
-    this.ai = autoPlayer;
-
-    // public : initialize the game current state to empty board configuration
-    this.currentState = new State();
-
-    //"E" stands for empty board cell
-    this.currentState.board = ["E", "E", "E",
-                               "E", "E", "E",
-                               "E", "E", "E"];
-
-    this.currentState.turn = "X"; //X plays first
-
+class Game {
     /*
-     * initialize game status to beginning
+     * Constructs a game object to be played
+     * @param autoPlayer [AIPlayer] : the AI player to be play the game with
      */
-    this.status = "beginning";
+    constructor(autoPlayer) {
+
+        //public : initialize the ai player for this game
+        this.ai = autoPlayer;
+
+        // public : initialize the game current state to empty board configuration
+        this.currentState = new State();
+
+        //"E" stands for empty board cell
+        this.currentState.board = ["E", "E", "E",
+                                   "E", "E", "E",
+                                   "E", "E", "E"];
+
+        this.currentState.turn = "X"; //X plays first
+
+        /*
+         * initialize game status to beginning
+         */
+        this.status = "beginning";
+    }
 
     /*
      * public function that advances the game to a new state
-     * @param _state [State]: the new state to advance the game to
+     * @param state [State]: the new state to advance the game to
      */
-    this.advanceTo = function(_state) {
-        this.currentState = _state;
-        if(_state.isTerminal()) {
+    advanceTo(state) {
+        this.currentState = state;
+        if(state.isTerminal()) {
             this.status = "ended";
 
-            if(_state.result === "X-won")
+            if(state.result === "X-won")
                 //X won
                 ui.switchViewTo("won");
-            else if(_state.result === "O-won")
+            else if(state.result === "O-won")
                 //X lost
                 ui.switchViewTo("lost");
             else
@@ -58,12 +60,12 @@ var Game = function(autoPlayer) {
                 this.ai.notify("O");
             }
         }
-    };
+    }
 
     /*
      * starts the game
      */
-    this.start = function() {
+    start() {
         if(this.status = "beginning") {
             //invoke advanceTo with the initial state
             this.advanceTo(this.currentState);
@@ -75,17 +77,17 @@ var Game = function(autoPlayer) {
 
 /*
  * public static function that calculates the score of the x player in a given terminal state
- * @param _state [State]: the state in which the score is calculated
+ * @param state [State]: the state in which the score is calculated
  * @return [Number]: the score calculated for the human player
  */
-Game.score = function(_state) {
-    if(_state.result === "X-won"){
+Game.score = function(state) {
+    if(state.result === "X-won"){
         // the x player won
-        return 10 - _state.oMovesCount;
+        return 10 - state.oMovesCount;
     }
-    else if(_state.result === "O-won") {
+    else if(state.result === "O-won") {
         //the x player lost
-        return - 10 + _state.oMovesCount;
+        return - 10 + state.oMovesCount;
     }
     else {
         //it's a draw
