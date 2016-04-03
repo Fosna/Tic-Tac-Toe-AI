12a5416b0309f4bf35e47c4b98187e3b9f42faa4
Fosna <vedran.marsic.ka@gmail.com>
2016-04-03 15:41:25 +0200

    Can work with scope in controller.

--- src/Tic-Tac-Toe-AI/app/difficulty/difficultyController.ts
+++ src/Tic-Tac-Toe-AI/app/difficulty/difficultyController.ts
@@ -2,8 +2,14 @@ import * as angular from "angular";
 
 console.log("difficulty controller first line");
 
+interface IDifficultyScope extends angular.IScope {
+    msg: string;
+}
+
 const difficultyController = function(app: angular.IModule) {
-    app.controller("DifficultyController", [function() {
+    app.controller("DifficultyController", ["$scope", function($scope: IDifficultyScope) {
+        $scope.msg = "Difficulty controller message.";
+        
         console.log("Angular is creating controller");
     }]);
     console.log("difficultyController");
