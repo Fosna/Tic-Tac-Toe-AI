ceb4655e93e457340eb5b2018322c14d21309c66
Fosna <vedran.marsic.ka@gmail.com>
2016-04-03 16:23:26 +0200

    Can change classes on difficulty picker.

--- src/Tic-Tac-Toe-AI/app/difficulty/difficultyController.ts
+++ src/Tic-Tac-Toe-AI/app/difficulty/difficultyController.ts
@@ -1,18 +1,28 @@
 import * as angular from "angular";
 
-console.log("difficulty controller first line");
-
 interface IDifficultyScope extends angular.IScope {
-    msg: string;
+    difficulty: string;
+    isBlind: boolean;
+    isNovice: boolean;
+    isMaster: boolean;
+    
+    setDifficulty(difficultyLevel: string);
+    getDifficultyClass(difficultyLevel: string);
 }
 
 const difficultyController = function(app: angular.IModule) {
     app.controller("DifficultyController", ["$scope", function($scope: IDifficultyScope) {
-        $scope.msg = "Difficulty controller message.";
+        $scope.difficulty = "";
+        $scope.isBlind = false;
         
-        console.log("Angular is creating controller");
+        $scope.setDifficulty = function(difficultyLevel) {
+            this.difficulty = difficultyLevel;
+            
+            this.isBlind = difficultyLevel === "blind";
+            this.isNovice = difficultyLevel === "novice";
+            this.isMaster = difficultyLevel === "master";
+        };
     }]);
-    console.log("difficultyController");
 };
 
 export default difficultyController;
