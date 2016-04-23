a3a838c20735a2fe20a87e1c8d6120604551995c
Fosna <vedran.marsic.ka@gmail.com>
2016-04-23 18:20:29 +0200

    Game status html changes based on real game status.

--- src/Tic-Tac-Toe-AI/app/component/gameStatus.js
+++ src/Tic-Tac-Toe-AI/app/component/gameStatus.js
@@ -3,21 +3,46 @@ import Difficulty from "./difficulty.js";
 
 const GameStatus = React.createClass({
     render: function () {
+        const gameStatus = this.props.gameStatus;
+        
+        let gameStatusComponent = <Difficulty />
+        if (gameStatus) {
+            console.log(gameStatus);
+            
+            gameStatusComponent = (<h1> { gameStatus } </h1>);
+            
+            switch(gameStatus) {
+                case "human":
+                    gameStatusComponent = (<div>It's your turn ...</div>);
+                    break;
+                case "ai":
+                    gameStatusComponent = (
+                        <div>
+                            <img src = 'imgs/robot.png' id = 'robot' class = 'robot' />
+                            <p>Wait for it ...</p>
+                        </div>
+                        );
+                 // TODO: check status magic strings
+                case "x-won":
+                    gameStatusComponent = (<div>You won !</div>);
+                    break;
+                case "o-won":
+                    gameStatusComponent = (<div>You lost !</div>); 
+                    break;
+                case "draw":
+                    gameStatusComponent = (<div>It's a Draw</div>);
+                    break;
+                default:
+                    throw { 
+                        message: "Invalid game status!",
+                        gameStatus: gameStatus  
+                    };
+            }
+        } 
+        
         return (
             <div className='control'>
-                
-                <h1>{ this.props.gameStatus }</h1>
-                
-                <Difficulty />
-                    
-                <div>It's your turn ...</div>
-                <div>
-                    <img src = 'imgs/robot.png' id = 'robot' class = 'robot' />
-                    <p>Wait for it ...</p>
-                </div>
-                <div>You won !</div>
-                <div>You lost !</div>
-                <div>It's a Draw</div>
+                { gameStatusComponent }
             </div>
         );
     }
