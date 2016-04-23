899ec6864555c95a21ef997fe48127df263f8f2a
Fosna <vedran.marsic.ka@gmail.com>
2016-04-23 15:49:46 +0200

    Little bit of refactoring. React default imports are used. Folders renamed.

--- src/Tic-Tac-Toe-AI/app/app.js
+++ src/Tic-Tac-Toe-AI/app/app.js
@@ -1,6 +1,7 @@
-import * as React from "react"; // Required for JSX syntax.
-import * as ReactDom from "react-dom";
-import TicTacToe from "./components/ticTacToe.js";
+import React from "react"; // Required for JSX syntax.
+import ReactDom from "react-dom";
+import TicTacToe from "./component/ticTacToe.js";
+import Dispatcher from "./dispatcher.js";
 
 var mountNode = document.getElementById("reactMountNode");
 ReactDom.render(<TicTacToe />, mountNode);
--- src/Tic-Tac-Toe-AI/app/component/ticTacToe.js
+++ src/Tic-Tac-Toe-AI/app/component/ticTacToe.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Difficulty from "./difficulty.js";
 
 const TicTacToe = React.createClass({
