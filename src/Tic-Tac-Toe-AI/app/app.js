5487a5bb5d4f8c1f90c21a821b5a29f47a57ed40
Fosna <vedran.marsic.ka@gmail.com>
2016-04-23 09:46:26 +0200

    Main app component wired up.

--- src/Tic-Tac-Toe-AI/app/app.js
+++ src/Tic-Tac-Toe-AI/app/app.js
@@ -1,13 +1,7 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
-
-var Monkey = React.createClass({
-    render: function () {
-        return (
-            <h1>Monkey</h1>
-        );
-    }
-});
+import TicTacToe from "./components/ticTacToe.js";
+// import Difficulty from "./components/difficulty.js";
 
 var mountNode = document.getElementById("reactMountNode");
-ReactDom.render(<Monkey />, mountNode);
+ReactDom.render(<TicTacToe />, mountNode);
---
+++ src/Tic-Tac-Toe-AI/app/components/ticTacToe.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+var TicTacToe = React.createClass({
+    render: function () {
+        return (
+            <h1>Monkey</h1>
+        );
+    }
+});
+
+export default TicTacToe;
