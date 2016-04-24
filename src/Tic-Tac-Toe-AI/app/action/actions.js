47e7a44a7d76d73a2fa241bafc50494aa782243b
Fosna <vedran.marsic.ka@gmail.com>
2016-04-24 22:30:46 +0200

    Actions singletone refactoring.

--- src/Tic-Tac-Toe-AI/app/action/actions.js
+++ src/Tic-Tac-Toe-AI/app/action/actions.js
@@ -1,24 +1,29 @@
 import Dispatcher from "../dispatcher.js";
 import * as ActionTypes from "./actionTypes.js";
 
-class Actions {
+class ActionsSingleton {
+    constructor(dispatcher) {
+        this.dispatcher = dispatcher;
+    }
     
+    difficultySet(difficulty) {
+        const action = {
+            actionType: ActionTypes.DIFFICULTY_SET,
+            difficulty: difficulty
+        };
+        this.dispatcher.dispatch(action);
+    }
+
+    cellClicked(index) {
+        const action = {
+            actionType: ActionTypes.CELL_CLICKED,
+            index: index
+        };
+        this.dispatcher.dispatch(action); 
+    }    
 }
 
-Actions.difficultySet = function(difficulty) {
-    const action = {
-        actionType: ActionTypes.DIFFICULTY_SET,
-        difficulty: difficulty
-    };
-    Dispatcher.dispatch(action);
-};
 
-Actions.cellClicked = function (index) {
-    const action = {
-        actionType: ActionTypes.CELL_CLICKED,
-        index: index
-    };
-    Dispatcher.dispatch(action); 
-};
+const Actions = new ActionsSingleton(Dispatcher);
 
 export default Actions;
